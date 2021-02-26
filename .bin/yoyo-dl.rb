22062badb697f8a9ed8ef97d6dc65b0751ca03ec
Sleepful <josepablov@gmail.com>
2021-02-26 04:05:48 -0600

    I did it! 149 anki notes

--- .bin/yoyo-dl.rb
+++ .bin/yoyo-dl.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/ruby
 
 for pinyin in ARGV
+  dPinyin = pinyin.downcase
   for tone in 1..4
-    `curl https://www.yoyochinese.com/audio/pychart/#{pinyin}#{tone}.mp3 --output #{pinyin}#{tone}.mp3`
+    `curl https://www.yoyochinese.com/audio/pychart/#{dPinyin}#{tone}.mp3 --output #{dPinyin}#{tone}.mp3`
   end
 end
 
