fac523b5eba32b61104c3a6c0f8d286362f5ded6
Sleepful <josepablov@gmail.com>
2021-03-14 01:37:50 -0600

    emojify pls

---
+++ .doom.d/custom.el
@@ -0,0 +1,29 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f4876796ef5ee9c82b125a096a590c9891cec31320569fc6ff602ff99ed73dca" default))
+ '(emojify-emoji-styles '(unicode))
+ '(org-agenda-files '("~/.doom.d/todos.org"))
+ '(pyim-dicts nil))
+;(custom-set-faces
+; ;; custom-set-faces was added by Custom.
+; ;; If you edit it by hand, you could mess it up, so be careful.
+; ;; Your init file should contain only one such instance.
+; ;; If there is more than one, they won't work right.
+; '(hl-line ((t (:underline "light slate blue" :background nil :distant-foreground "medium purple" :distant-background "black"))))
+; '(line-number-current-line ((t (:foreground "cyan"))))
+; '(region ((t (:background "VioletRed4" :distant-foreground "green"))))
+; '(solaire-hl-line-face ((t (:background nil))))
+; '(solaire-mode-line-face ((t (:background "black" :underline nil))))
+; '(solaire-mode-line-inactive-face ((t (:background "#190e22" :foreground "medium aquamarine"))))
+; '(web-mode-interpolate-color1-face ((t (:foreground "peach puff"))))
+; '(web-mode-javascript-string-face ((t (:foreground "medium aquamarine")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
