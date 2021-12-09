9b70669b76365df9d93d91cb4f3defb42476e393
Sleepful <josepablov@gmail.com>
2021-12-09 03:35:14 -0600

    uh huh

--- .bin/install/install.sh
+++ .bin/install/install.sh
@@ -9,6 +9,8 @@ cfg restore .zshrc
 # Emacs
 brew install --no-quarantine emacs --HEAD
 /usr/bin/tic -x -o ~/.terminfo ~/.bin/install/config/terminfo-custom.src
+# Doom
+rm -rf ~/.emacs.d
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 
