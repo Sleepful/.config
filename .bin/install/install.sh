7bc1b6a5156eca043977a7fbd7f9273cba0287bc
Sleepful <josepablov@gmail.com>
2021-12-09 18:22:33 -0600

    nvm

--- .bin/install/install.sh
+++ .bin/install/install.sh
@@ -38,6 +38,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 14
+# nvm alias default 14
 nvm use node
 
 # elixir
