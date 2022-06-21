dae39e86da4e9690b487339b3046b90c3bdc8915
Sleepful <josepablov@gmail.com>
2022-06-20 23:58:19 -0600

    elixir stuff

--- .bin/install/install-elixir.sh
+++ .bin/install/install-elixir.sh
@@ -4,7 +4,20 @@
 brew install asdf
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+# erlang pre-reqs, must be there at the
+# time that erlang gets compiled
+brew install wxmac
+brew install unixodbc # seems like this isn't working
+# because the erlang install command says obdc check failed
+brew install fop # for pdf documentation generation
 asdf install erlang latest
 asdf install elixir latest
 asdf global erlang latest
 asdf global elixir latest
+
+# must do something like:
+# echo "erlang 25.0.1" >> .tool-versions
+# in order to run `$ mix` command
+
+mix local.hex
+mix archive.install hex phx_new
