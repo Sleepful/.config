c8ed323fcd5694984c4e42a21569dff8b45047dc
Sleepful <josepablov@gmail.com>
2023-05-02 23:03:11 -0600

    nvim: sync last yank register

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("sync_last_yank_with_l_register"),
+  callback = function()
+    local last_yank = vim.fn.getreg("0")
+    vim.fn.setreg("l", last_yank)
+  end,
+})
