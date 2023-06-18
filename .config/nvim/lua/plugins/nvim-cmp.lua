13642365125747957fb12fdeeebc5f720e219c46
Sleepful <josepablov@gmail.com>
2023-06-18 02:57:07 -0600

    Nvim-cmp: <C-t> includes sources data

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -47,35 +47,79 @@ return {
           ["<C-l>"] = vim.NIL, -- NOTE: this was changed from default config
           -- ["<C-n>"] = vim.NIL,
           ["<C-t>"] = function(fallback)
+            -- TODO:
+            -- [ ] - does not work great with snippets yet
+            -- [ ] - does not preview anything, should preview docs for LSP and snippets for snipets
             if cmp.visible() then
               -- adapted from:
               -- https://github.com/hrsh7th/nvim-cmp/blob/main/lua/cmp/core.lua#L355
               -- https://github.com/nvim-telescope/telescope.nvim/blob/master/developers.md#replacing-actions
-              local labels = {}
               local entries = cmp.get_entries()
-              for _, e in ipairs(entries) do
-                table.insert(labels, e.completion_item.label)
-              end
               local pickers = require("telescope.pickers")
               local finders = require("telescope.finders")
               local conf = require("telescope.config").values
               local actions = require("telescope.actions")
               local action_state = require("telescope.actions.state")
+              local displayer = require("telescope.pickers.entry_display").create({
+                separator = "▏",
+                items = {
+                  { width = 6 },
+                  { width = 12 },
+                  { remaining = true },
+                },
+              })
+
+              local fmt_source_name = function(name)
+                if name == "nvim_lsp" then
+                  return "LSP"
+                end
+                if name == "buffer" then
+                  return "Text"
+                end
+                if name == "luasnip" then
+                  return "Snip"
+                end
+                return name
+              end
+
+              local fmt_source_kind = function(kind)
+                if kind == "" or kind == nil then
+                  return " "
+                end
+                local types = require("cmp.types")
+                return types.lsp.CompletionItemKind[kind]
+              end
+
               local telescope_cmp = function(opts)
                 opts = opts or {}
                 pickers
                   .new(opts, {
                     prompt_title = "Auto CMP",
                     finder = finders.new_table({
-                      results = labels,
+                      results = entries,
+                      entry_maker = function(entry)
+                        return {
+                          value = entry,
+                          ordinal = fmt_source_name(entry.source.name) .. " " .. fmt_source_kind(
+                            entry.completion_item.kind
+                          ) .. " " .. entry.completion_item.label,
+                          display = function(e)
+                            return displayer({
+                              fmt_source_name(e.value.source.name),
+                              fmt_source_kind(e.value.completion_item.kind),
+                              e.value.completion_item.label,
+                            })
+                          end,
+                        }
+                      end,
                     }),
                     sorter = conf.generic_sorter(opts),
                     attach_mappings = function(prompt_bufnr, map)
                       actions.select_default:replace(function()
                         actions.close(prompt_bufnr)
                         local selection = action_state.get_selected_entry()
-                        local e = entries[selection.index]
-                        print(vim.inspect(e.completion_item.label))
+                        local e = selection.value
+                        print(vim.inspect(e.completion_item))
                         vim.schedule(function()
                           vim.cmd([[silent! undojoin]])
                           -- This logic must be used nvim_buf_set_text.
