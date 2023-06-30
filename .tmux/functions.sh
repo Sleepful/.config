9ccf77fef9c237a1c1ea0b5311de01b88ac25123
Sleepful <josepablov@gmail.com>
2023-06-30 00:18:54 -0600

    tmux: move_windows

---
+++ .tmux/functions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+function move_windows() {
+	windows=$(tmux list-windows -F "#{window_active} #{window_index} #{window_name}" | sort -r | awk '{print $2 " : " $3}' | fzf-tmux -p | awk '{print $1}' | sort -r)
+	if windows; then
+		session=$(tmux list-sessions -F "#{session_name}" | fzf-tmux -p | awk '{print $1}')
+		current_session=$(tmux display-message -p '#S')
+		echo $windows | xargs -n1 -I {i} tmux move-window -b -s "$current_session":{i} -t "$session":1
+	fi
+}
