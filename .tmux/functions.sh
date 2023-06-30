6c5f0d97b308e87686a9df0ae59b5923014bac91
Sleepful <josepablov@gmail.com>
2023-06-30 00:27:30 -0600

    Tmux: move_windows fix

--- .tmux/functions.sh
+++ .tmux/functions.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
 function move_windows() {
-	windows=$(tmux list-windows -F "#{window_active} #{window_index} #{window_name}" | sort -r | awk '{print $2 " : " $3}' | fzf-tmux -p | awk '{print $1}' | sort -r)
-	if windows; then
+	windows=$(
+		tmux list-windows -F "#{window_active} #{window_index} #{window_name}" | sort -r | awk '{print $2 " : " $3}' | fzf-tmux -p | awk '{print $1}' | sort -r
+	)
+	if [ -n "$windows" ]; then
 		session=$(tmux list-sessions -F "#{session_name}" | fzf-tmux -p | awk '{print $1}')
 		current_session=$(tmux display-message -p '#S')
 		echo $windows | xargs -n1 -I {i} tmux move-window -b -s "$current_session":{i} -t "$session":1
