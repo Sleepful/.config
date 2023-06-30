43f95f9723356cf7c374ac4ce2a1a7659a133e93
Sleepful <josepablov@gmail.com>
2023-06-30 00:49:38 -0600

    Tmux: better session picker;
    First option will be the current session

--- .tmux/functions.sh
+++ .tmux/functions.sh
@@ -5,8 +5,11 @@ function move_windows() {
 		tmux list-windows -F "#{window_active} #{window_index} #{window_name}" | sort -r | awk '{print $2 " : " $3}' | fzf-tmux -p | awk '{print $1}' | sort -r
 	)
 	if [ -n "$windows" ]; then
-		session=$(tmux list-sessions -F "#{session_name}" | fzf-tmux -p | awk '{print $1}')
+		session=$(tmux list-sessions -F '#{session_attached} #{session_name}' | sort -r | awk '{print $2}' | fzf-tmux -p | awk '{print $1}')
 		current_session=$(tmux display-message -p '#S')
 		echo $windows | xargs -n1 -I {i} tmux move-window -b -s "$current_session":{i} -t "$session":1
 	fi
+}
+function session_picker() {
+	tmux list-sessions -F '#{session_attached} #{session_name}' | sort -r | awk '{print $2}' | fzf-tmux -p | xargs tmux switchc -t
 }
