384dfd3774ec39603dbafc5a40610925038d4242
Sleepful <josepablov@gmail.com>
2023-11-17 17:22:03 -0600

    clojure

--- .config/nvim/lua/plugins/conjure.lua
+++ .config/nvim/lua/plugins/conjure.lua
@@ -1,2 +1,11 @@
-return { { "Olical/conjure" }, { "tpope/vim-dispatch" }, { "radenling/vim-dispatch-neovim" },
-  { "clojure-vim/vim-jack-in" } }
+return {
+  { "Olical/conjure" },
+  { "tpope/vim-dispatch" },
+  { "radenling/vim-dispatch-neovim" },
+  {
+    "clojure-vim/vim-jack-in",
+    config = function()
+      vim.g.default_lein_task = "with-profile dev repl"
+    end
+  }
+}
