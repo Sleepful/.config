4e28da729a4009349c5279347323862134340fbb
Sleepful <josepablov@gmail.com>
2023-11-18 21:49:43 -0600

    Add indentation options: 2 spaces

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -14,3 +14,9 @@ vim.opt.jumpoptions = "stack"
 vim.g.markdown_fenced_languages = {
 	"ts=typescript",
 }
+
+local o = vim.o
+o.expandtab = true   -- expand tab input with spaces characters
+o.smartindent = true -- syntax aware indentations for newline inserts
+o.tabstop = 2        -- num of space characters per tab
+o.shiftwidth = 2     -- spaces per indentation level
