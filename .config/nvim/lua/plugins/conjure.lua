4c0125f2f86abfd5885896c3ab50a106c4638ab7
Sleepful <josepablov@gmail.com>
2023-11-25 17:45:19 -0600

    Better guns/vim-sexp (paredit for nvim), explicit keybinds

--- .config/nvim/lua/plugins/conjure.lua
+++ .config/nvim/lua/plugins/conjure.lua
@@ -1,8 +1,69 @@
--- local clojure_filetype = function()
---   return vim.bo.filetype == "clojure"
--- end
+-- NOTE: maybe consider adding this:
+--    https://github.com/guns/vim-sexp
+-- or this:
+--    https://github.com/julienvincent/nvim-paredit
+--
+--    good paredit functionality to add:
+--      forward/backwards slurp
+--      forward/backwards barf
+--
+--      forward-down (navigation)
+--      backward-up (navigation)
+--
+--      forwards/backwards splice-and-kill
+--
+--      split/join sexp
+--
+--      look here: https://github.com/guns/vim-sexp/issues/5
+
+
 local clojure_filetype = "clojure"
 return {
+  {
+    "guns/vim-sexp",
+    ft = clojure_filetype,
+    init = function()
+      -- Disable mapping hooks
+      vim.g.sexp_filetypes = ''
+      -- these are taken from the docs:
+      --    https://github.com/guns/vim-sexp/blob/master/doc/vim-sexp.txt#L647
+      --    *sexp-explicit-mappings*
+      vim.cmd([=[
+        function! s:vim_sexp_mappings()
+            nmap <silent><buffer> (               <Plug>(sexp_move_to_prev_bracket)
+            xmap <silent><buffer> (               <Plug>(sexp_move_to_prev_bracket)
+            omap <silent><buffer> (               <Plug>(sexp_move_to_prev_bracket)
+            nmap <silent><buffer> )               <Plug>(sexp_move_to_next_bracket)
+            xmap <silent><buffer> )               <Plug>(sexp_move_to_next_bracket)
+            omap <silent><buffer> )               <Plug>(sexp_move_to_next_bracket)
+            imap <silent><buffer> <BS>            <Plug>(sexp_insert_backspace)
+            imap <silent><buffer> "               <Plug>(sexp_insert_double_quote)
+            imap <silent><buffer> (               <Plug>(sexp_insert_opening_round)
+            imap <silent><buffer> )               <Plug>(sexp_insert_closing_round)
+            imap <silent><buffer> [               <Plug>(sexp_insert_opening_square)
+            imap <silent><buffer> ]               <Plug>(sexp_insert_closing_square)
+            imap <silent><buffer> {               <Plug>(sexp_insert_opening_curly)
+            imap <silent><buffer> }               <Plug>(sexp_insert_closing_curly)
+          endfunction
+              augroup VIM_SEXP_MAPPING
+              autocmd!
+              autocmd FileType clojure,scheme,lisp,timl call s:vim_sexp_mappings()
+          augroup END
+      ]=])
+    end,
+    keys = {
+      {
+        "<M-[>", -- overwrites default
+        function()
+          vim.cmd([[execute "normal \<Plug>(sexp_flow_to_prev_open)"]])
+        end },
+      {
+        "<M-]>", -- same as default
+        function()
+          vim.cmd([[execute "normal \<Plug>(sexp_flow_to_next_open)"]])
+        end },
+    }
+  },
   {
     "Olical/conjure",
     ft = clojure_filetype,
