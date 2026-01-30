02581d0ec2ff7bf5f4495c9f7743fdafec940205
Jose Vargas <josepablov@gmail.com>
2026-01-30 13:57:11 -0600

    add opencode ding for user input

--- .config/opencode/plugin/terminal-bell.ts
+++ .config/opencode/plugin/terminal-bell.ts
@@ -3,7 +3,8 @@ import type { Plugin } from "@opencode-ai/plugin"
 export const TerminalBell: Plugin = async ({ project, client, $, directory, worktree }) => {
   return {
     event: async ({ event }) => {
-      if (event.type === "session.idle") {
+      // console.error("Event:", event.type) // `tail -f events.log` to view this
+      if (event.type === "session.idle" || event.type === "question.asked") {
         await Bun.write(Bun.stdout, "\x07")
         try {
           await $`afplay /System/Library/Sounds/Ping.aiff`;
